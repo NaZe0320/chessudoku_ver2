44e28542c5ca4c354d616e6702af07a37c5d72c8
NaZe0320 <rwt1226@naver.com>
2025-06-12 12:51:32 +0900

    [feat] 퍼즐 팩 관리 프로바이더 추가 및 추천 팩 탭에서 상태 관리 개선

--- lib/core/di/puzzle_pack_provider.dart
+++ lib/core/di/puzzle_pack_provider.dart
@@ -1,8 +1,18 @@
+import 'package:chessudoku/core/di/providers.dart';
 import 'package:chessudoku/domain/notifiers/filter_notifier.dart';
+import 'package:chessudoku/domain/notifiers/puzzle_pack_notifier.dart';
 import 'package:chessudoku/domain/states/filter_state.dart';
+import 'package:chessudoku/domain/states/puzzle_pack_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final recommendPackTypeFilterProvider =
     StateNotifierProvider<FilterNotifier<String>, FilterState<String>>((ref) {
   return FilterNotifier<String>();
+});
+
+/// 퍼즐 팩 관리 Provider
+final puzzlePackNotifierProvider =
+    StateNotifierProvider<PuzzlePackNotifier, PuzzlePackState>((ref) {
+  final puzzleRepository = ref.watch(puzzleRepositoryProvider);
+  return PuzzlePackNotifier(puzzleRepository);
 });
